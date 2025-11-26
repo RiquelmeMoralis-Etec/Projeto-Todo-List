8ffcdbdc188719b13639d625c16193ab90026286
RiquelmeMoralis-Etec <riquelme.moralis.etec@gmail.com>
2025-11-25 22:02:07 -0300

    Delete BackEnd/auth.py

--- BackEnd/auth.py
+++
@@ -1,9 +0,0 @@
-# auth.py
-
-from werkzeug.security import generate_password_hash, check_password_hash
-
-# Criando hash de senha
-hashed_pw = generate_password_hash("minhasenha123")
-
-# Verificando senha
-check_password_hash(hashed_pw, "minhasenha123")  # True
